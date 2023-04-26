7425ff0fd103d188e58235340b2ccf661888c82e
Jonah TzCh <jonah_mytzuchi@outlook.com>
2023-04-26 17:55:23 +0800

    implementation photo, video, document, mediaGroup

--- bot/bot.py
+++ bot/bot.py
@@ -5,78 +5,105 @@ import config
 import telegram_utils as tu
 
 TOKEN = config.telegram_token
-bot = telegram.Bot(token=TOKEN)
+CDN = "https://jonahtzuchi.github.io/Python-Telegram-Broadcast-Bot"
+
+# Most we can't test all of the test-case at once due to request frequency set by Telegram
+# Uncomment code you would like to test
 
 async def main():
-    # case-1, send text message
-    USER_TELEGRAM_ID = 75316412
-    message = "testing"
-    #await tu.sendMessage(bot, USER_TELEGRAM_ID, message)
-    # case-2, send photo
-    image_name = "kawaii_asian_girl.jpeg"
-    USER_TELEGRAM_ID = 75316412
-    #await tu.sendPhoto(bot, USER_TELEGRAM_ID, image_name)
-    # case-3, send photo by file_id
-    image_1 = (
-        "AgACAgUAAxkDAANOZEdwPMoxw__lBUt5Wgl-vCzicr8AAjW1MRtvGjhWDqzDe-GjbqwBAAMCAANzAAMvBA"
-    )
-    USER_TELEGRAM_ID = 75316412
-    #await tu.sendPhotoById(bot, USER_TELEGRAM_ID, image_1)
-    # case-4, send photo in a group
-    image_1 = (
-        "AgACAgUAAxkDAANOZEdwPMoxw__lBUt5Wgl-vCzicr8AAjW1MRtvGjhWDqzDe-GjbqwBAAMCAANzAAMvBA"
-    )
-    image_2 = (
-        "AgACAgUAAxkDAANdZEeLPA0ND1x3VINM34sZYgOe9GQAAim1MRtvGjhWUp8OPHix_NMBAAMCAANzAAMvBA"
-    )
-    image_3 = (
-        "AgACAgUAAxkDAANcZEeLO0hw-e1YZxdRVZLM2P33UUAAAjW1MRtvGjhWDqzDe-GjbqwBAAMCAANzAAMvBA"
-    )
-    image_4 = (
-        "AgACAgUAAxkDAANjZEeL_EMjTy9e2JiZ6zGL7mu81aYAAjK1MRtvGjhWMwiQuXvyGywBAAMCAANzAAMvBA"
-    )
-    USER_TELEGRAM_ID = 75316412
-    media_collection = [
-        telegram.InputMediaPhoto(media=image_1),
-        telegram.InputMediaPhoto(media=image_2),
-        telegram.InputMediaPhoto(media=image_3),
-        telegram.InputMediaPhoto(media=image_4),
-    ]
-    #await tu.sendMediaGroup(bot, USER_TELEGRAM_ID, media_collection)
-    print("END")
-    # case-5, send doc
-    USER_TELEGRAM_ID = 75316412
-    doc = "01. The A-Z of Programming Languages author Computerworld.pdf"
-    #await tu.sendDocument(bot, USER_TELEGRAM_ID, open(f"./static/{doc}", "rb"))
-    # case-6, send video
-    USER_TELEGRAM_ID = 75316412#105816202#
-    video_1 = "pexels-life-on-super-3324257-1920x1080-18fps.mp4"
-    video_2 = "pexels-life-on-super-3588875-1280x720-18fps.mp4"
-    video_3 = "pexels-taryn-elliott-3018669-1920x1080-24fps.mp4" # 10MB++
-    video_1_id = "BAACAgUAAxkDAAOOZEed3qRwGHMIE93UYeOcZMieGnkAAnQMAAJvGkBW-cekLfejplkvBA"
-    video_2_id = "BAACAgUAAxkDAAOQZEee478uvQnIW8DdUjInfdSDz0AAAnUMAAJvGkBWF195utnoAuQvBA"
-    video_3_id = "BAACAgUAAxkDAAOTZEegDHOIYI19tueFBQRvdbAa1TMAAuwIAAISVDlWRm-WtzG5M0YvBA"
+    # Initialize bot
+    bot = telegram.Bot(token=TOKEN)
+    # valid telegram user id in integer format
+    # just replace config.target below to telegram id of your receiver
+    # before this make sure 
+    ### the receiver's telegram account search for your bot
+    ### the user click the "start" button
+    # otherwise, the user won't be able to receive the content you are about to send
+    RECEIVER_TELEGRAM_ID = config.target_id #replace config.target to telegram id of your receiver
 
-    #await tu.sendVideo(bot, USER_TELEGRAM_ID, open(f"./static/{video_3}", "rb"))
-    #await tu.sendVideo(bot, USER_TELEGRAM_ID, video_3_id)
+    '''
+        case-1, send message
+    '''
+    #print("sendMessage")
+    #message = "Good day"
+    #await tu.sendMessage(bot, RECEIVER_TELEGRAM_ID, message)
 
-    #case-7, mix type, failed
-    doc = "01. The A-Z of Programming Languages author Computerworld.pdf"
-    video_1 = "pexels-life-on-super-3324257-1920x1080-18fps.mp4"
-    media_collection = [
-        telegram.InputMediaDocument(media=open(f"./static/{doc}", "rb")),
-        telegram.InputMediaVideo(media=open(f"./static/{video_1}", "rb"))
-    ]
-    #await tu.sendMediaGroup(bot, USER_TELEGRAM_ID, media_collection)
-    #case-8, video collection
-    video_1_id = "BAACAgUAAxkDAAOOZEed3qRwGHMIE93UYeOcZMieGnkAAnQMAAJvGkBW-cekLfejplkvBA"
-    video_2_id = "BAACAgUAAxkDAAOQZEee478uvQnIW8DdUjInfdSDz0AAAnUMAAJvGkBWF195utnoAuQvBA"
-    video_3_id = "BAACAgUAAxkDAAOTZEegDHOIYI19tueFBQRvdbAa1TMAAuwIAAISVDlWRm-WtzG5M0YvBA"
-    media_collection = [
-        telegram.InputMediaVideo(media=video_1_id),
-        telegram.InputMediaVideo(media=video_2_id),
-        telegram.InputMediaVideo(media=video_3_id)
-    ]
-    await tu.sendMediaGroup(bot, USER_TELEGRAM_ID, media_collection)
+    '''
+        Case-2, send photo
+    '''
+    #print("send photo raw file")
+    #image_name = f"./static/kawaii_asian_girl.jpeg" # make sure photo you choose is placed under folder 'static'
+    #image_id = await tu.sendPhoto(bot, RECEIVER_TELEGRAM_ID, image_name, return_id = True)
+
+    #print("send photo URL")
+    #image_url = f"{CDN}/static/kawaii_asian_girl.jpeg"
+    #image_id = await tu.sendPhoto(bot, RECEIVER_TELEGRAM_ID, image_url, return_id = True)
+
+    #print("send photo file_id") # id included below might not work for you due to 
+    #_image_id = "AgACAgQAAxkDAAO5ZEjRO_4h0ozAaZTNeHd85rYvFkcAAmqvMRv-00xS62nhWuOgCekBAAMCAANzAAMvBA"
+    #image_id = await tu.sendPhoto(bot, RECEIVER_TELEGRAM_ID, _image_id, "kawaii", True)
+    #assert _image_id != image_id, f"\n{_image_id}\n{image_id}"
+    #print("Returned image id:", image_id)
+
+    '''
+        case-3, send video
+    '''
+    #print("send video raw file")
+    #video_name = "./static/pexels-life-on-super-3324257-1920x1080-18fps.mp4"
+    #video_id = await tu.sendVideo(bot, RECEIVER_TELEGRAM_ID, video_name, "raw file", True)
+    #print("Returned video id", video_id)
+    
+    #print("send video URL")
+    #video_url = f"{CDN}/static/pexels-life-on-super-3324257-1920x1080-18fps.mp4"
+    #video_id = await tu.sendVideo(bot, RECEIVER_TELEGRAM_ID, video_url, "URL", True)
+    #print("Returned video id", video_id)
+    
+    #print("send video file_id")
+    #video_id = "BAACAgQAAxkDAAPHZEjdgjIpTYcKV0DiFzKkp_a3iw8AAkgEAAI3a0VSZ-MYCql5ZFUvBA"
+    #video_id = await tu.sendVideo(bot, RECEIVER_TELEGRAM_ID, video_id, "file id", True)
+    #print("Returned video id", video_id)
+
+    '''
+        case-4, send document
+    '''
+    #print("send document raw file")
+    #document_name = "./static/01. The A-Z of Programming Languages author Computerworld.pdf"
+    #document_id = await tu.sendDocument(bot, RECEIVER_TELEGRAM_ID, document_name, "raw file", True)
+    #print("Returned document id", document_id)
+    
+    #print("send document URL")
+    #document_url = f"{CDN}/static/01. The A-Z of Programming Languages author Computerworld.pdf"
+    #document_id = await tu.sendDocument(bot, RECEIVER_TELEGRAM_ID, document_url, "URL", True)
+    #print("Returned document id", document_id)
+
+    #print("send document file_id")
+    #document_id = "BQACAgQAAxkDAAPLZEjpAc2NLcLsow-ZL-N7elVNqKIAAtEDAALFrExSPqW1esNd0wkvBA"
+    #document_id = await tu.sendDocument(bot, RECEIVER_TELEGRAM_ID, document_id, "URL", True)
+    #print("Returned document id", document_id)
+
+    '''
+        case-5, send photo in a group
+    '''
+    #print("send photo in a group URL")
+    #image_1 = f"{CDN}/static/midjourney_blonde.jpeg"
+    #image_2 = f"{CDN}/static/midjourney_girl_by_cryingswan_dfjf806-fullview.jpg"
+    #image_3 = f"{CDN}/static/midjourney_girl_red-hat-smile.jpeg"
+    #media_collection = [
+    #    telegram.InputMediaPhoto(media=tu.file_handler(image_1), caption="image 1", filename="midjourney_blonde.jpeg"),
+    #    telegram.InputMediaPhoto(media=tu.file_handler(image_2), caption="image 2", filename="midjourney_girl_by_cryingswan_dfjf806-fullview.jpg"),
+    #    telegram.InputMediaPhoto(media=tu.file_handler(image_3), caption="image 3", filename="midjourney_girl_red-hat-smile.jpeg"),
+    #]
+    #res = await tu.sendMediaGroup(bot, RECEIVER_TELEGRAM_ID, media_collection)
+    #print(res)
+    
+    '''
+        case-6, send video in a group
+    '''
+    # change telegram.InputMediaPhoto -> telegram.InputMediaVideo
+
+    '''
+        case-7, send document in a group
+    '''
+    # change telegram.InputMediaPhoto -> telegram.InputMediaDocument
 
 asyncio.run(main())
--- bot/telegram_utils.py
+++ bot/telegram_utils.py
@@ -1,47 +1,238 @@
 import os
 import telegram
 
-
-async def sendPhoto(bot, target_id, image_name):
-    path = f"./static/{image_name}"
-    res = await bot.send_photo(
-        chat_id=target_id,
-        photo=open(path, "rb"),
-        caption=image_name,
-        allow_sending_without_reply=True,
-    )  # reply_to_message_id=target_id,
-    print("res:", res["photo"][0]["file_id"])
+"""
+    protect_content : bool = False
+    any user can access to photo/video/doc we shared through file_id
+    protect_content = True will prevent other user from sharing or forwarding
+"""
 
 
-async def sendPhotoById(bot, target_id, file_id):
-    res = await bot.send_photo(
-        chat_id=target_id,
-        photo=file_id,
-        caption="through file id",
-        reply_to_message_id=target_id,
-        allow_sending_without_reply=True,
-    )
-
-
-async def sendMediaGroup(bot, target_id, collection):
-    await bot.send_media_group(
-        chat_id=target_id, media=collection, allow_sending_without_reply=True
-    )
-
-
-async def sendDocument(bot: telegram.Bot, target_id, doc):
-    res = await bot.sendDocument(
-        chat_id=target_id, document=doc, allow_sending_without_reply=True
-    )
-    print(res)
-
-
-async def sendVideo(bot: telegram.Bot, target_id, vid):
-    res = await bot.sendVideo(
-        chat_id=target_id, video=vid, allow_sending_without_reply=True
-    )
-    print(res)
+def file_handler(media):
+    # raw file
+    if type(media) != str:
+        return media
+    ext = media.split(".")
+    # URL
+    if "https://" in media or "http://" in media:
+        return media
+    if len(ext) > 1:
+        # file_path, a complete path is required
+        if ext[-1].lower() in ["jpg", "jpeg", "png", "mp4", "pdf"]:
+            return open(media, "rb")
+    # file_id
+    return media
 
 
 async def sendMessage(bot, target_id, message):
     res = await bot.sendMessage(chat_id=target_id, text=message)
+
+
+async def sendPhoto(
+    bot: telegram.Bot, target_id: str, photo: any, caption: str = None, return_id=False
+):
+    _photo = file_handler(photo)
+    res = await bot.send_photo(
+        chat_id=target_id,
+        photo=_photo,
+        caption=caption,
+        allow_sending_without_reply=True,
+        protect_content=False,
+    )
+    print(res)
+    """
+    # what I want to show here is photo[0]['file_id'] is equal to photo[1]['file_id']
+    # and many more information are captured here
+    Message(
+        caption='###', 
+        channel_chat_created=False, 
+        chat=Chat(first_name='###', id=###, type=<ChatType.PRIVATE>, username='###'), 
+        date=datetime.datetime(2023, 4, 26, 1, 14, 34, tzinfo=<UTC>), 
+        delete_chat_photo=False, 
+        from_user=User(first_name='###', id=###, is_bot=True, username='###_Bot'), 
+        group_chat_created=False, 
+        message_id=###, 
+        photo=(
+            PhotoSize(file_id='###', file_size=1431, file_unique_id='@@@@@@@@@@@@@@@@', height=90, width=62), 
+            PhotoSize(file_id='###', file_size=7267, file_unique_id='$$$$$$$$$$$$$$$$', height=272, width=186)
+        ), 
+        supergroup_chat_created=False
+    )
+    """
+    if return_id:
+        return res["photo"][0]["file_id"]
+
+
+async def sendVideo(
+    bot: telegram.Bot, target_id: str, video: any, caption: str = None, return_id=False
+):
+    _video = file_handler(video)
+    res = await bot.sendVideo(
+        chat_id=target_id,
+        video=_video,
+        caption=caption,
+        allow_sending_without_reply=True,
+        protect_content=False,
+    )
+    print(res)
+    """
+    Message(
+        caption='raw file', 
+        channel_chat_created=False, 
+        chat=Chat(first_name='###', id=###, type=<ChatType.PRIVATE>, username='###'), 
+        date=datetime.datetime(2023, 4, 26, 8, 9, 26, tzinfo=<UTC>), 
+        delete_chat_photo=False, 
+        from_user=User(first_name='###', id=###, is_bot=True, username='###_Bot'), 
+        group_chat_created=False, 
+        message_id=###, 
+        supergroup_chat_created=False, 
+        video=Video(
+            api_kwargs={
+                'thumbnail': {
+                    'file_id': '###', 
+                    'file_unique_id': '###', 
+                    'file_size': 9700, 'width': 320, 'height': 180
+                }
+            }, 
+            duration=4, 
+            file_id='###', 
+            file_name='pexels-life-on-super-3324257-1920x1080-18fps.mp4', 
+            file_size=1944465, 
+            file_unique_id='###', 
+            height=1080, 
+            mime_type='video/mp4', 
+            thumb=PhotoSize(
+                file_id='###', 
+                file_size=9700, 
+                file_unique_id='###', 
+                height=180, 
+                width=320
+            ), 
+            width=1920
+        )
+    )
+    """
+    if return_id:
+        return res["video"]["file_id"]
+
+
+async def sendDocument(
+    bot: telegram.Bot,
+    target_id: str,
+    document: any,
+    caption: str = None,
+    return_id=False,
+):
+    _document = file_handler(document)
+    res = await bot.sendDocument(
+        chat_id=target_id,
+        document=_document,
+        caption=caption,
+        allow_sending_without_reply=True,
+        protect_content=False,
+    )
+    print(res)
+    """
+    Message(
+        caption='raw file', 
+        channel_chat_created=False, 
+        chat=Chat(first_name='###', id=###, type=<ChatType.PRIVATE>, username='###'), 
+        date=datetime.datetime(2023, 4, 26, 8, 59, 32, tzinfo=<UTC>), 
+        delete_chat_photo=False, 
+        document=Document(
+            api_kwargs={
+                'thumbnail': {
+                    'file_id': '###', 
+                    'file_unique_id': '###', 
+                    'file_size': 12967, 
+                    'width': 226, 
+                    'height': 320
+                }
+            }, 
+            file_id='###', 
+            file_name='01. The A-Z of Programming Languages author Computerworld.pdf', 
+            file_size=945553, 
+            file_unique_id='###', 
+            mime_type='application/pdf', 
+            thumb=PhotoSize(
+                file_id='###', 
+                file_size=12967, 
+                file_unique_id='###', 
+                height=320, 
+                width=226
+            )
+        ), 
+        from_user=User(first_name='###', id=###, is_bot=True, username='###_Bot'), 
+        group_chat_created=False, 
+        message_id=###, 
+        supergroup_chat_created=False
+    )
+    """
+    if return_id:
+        return res["document"]["file_id"]
+
+
+async def sendMediaGroup(bot: telegram.Bot, target_id: str, collection: list):
+    res = await bot.send_media_group(
+        chat_id=target_id,
+        media=collection,
+        allow_sending_without_reply=True,
+        protect_content=False,
+    )
+    # print(res)
+    """
+    ### look for media_group_id, observe that they are the same
+    (
+        Message(
+            caption='image 1', 
+            channel_chat_created=False, 
+            chat=Chat(first_name='###', id=###, type=<ChatType.PRIVATE>, username='###'), 
+            date=datetime.datetime(2023, 4, 26, 9, 18, 30, tzinfo=<UTC>), 
+            delete_chat_photo=False, 
+            from_user=User(first_name='###', id=###, is_bot=True, username='###_Bot'), 
+            group_chat_created=False, 
+            media_group_id='###', 
+            message_id=###, 
+            photo=(
+                PhotoSize(file_id='###', file_size=1616, file_unique_id='###', height=90, width=90), 
+                PhotoSize(file_id='###', file_size=5818, file_unique_id='###', height=225, width=225)
+            ), 
+            supergroup_chat_created=False
+        ), 
+        Message(
+            caption='image 2', 
+            channel_chat_created=False, 
+            chat=Chat(first_name='###', id=###, type=<ChatType.PRIVATE>, username='###'), 
+            date=datetime.datetime(2023, 4, 26, 9, 18, 30, tzinfo=<UTC>), 
+            delete_chat_photo=False, 
+            from_user=User(first_name='###', id=###, is_bot=True, username='###_Bot'), 
+            group_chat_created=False, 
+            media_group_id='###', 
+            message_id=###, 
+            photo=(
+                PhotoSize(file_id='###', file_size=2084, file_unique_id='###', height=90, width=90), 
+                PhotoSize(file_id='###', file_size=27169, file_unique_id='###', height=320, width=320), 
+                PhotoSize(file_id='###', file_size=93640, file_unique_id='###', height=894, width=894), 
+                PhotoSize(file_id='###', file_size=100420, file_unique_id='###', height=800, width=800)
+            ), 
+            supergroup_chat_created=False
+        ), 
+        Message(
+            caption='image 3', 
+            channel_chat_created=False, 
+            chat=Chat(first_name='###', id=###, type=<ChatType.PRIVATE>, username='###'), 
+            date=datetime.datetime(2023, 4, 26, 9, 18, 30, tzinfo=<UTC>), 
+            delete_chat_photo=False, 
+            from_user=User(first_name='###', id=###, is_bot=True, username='###_Bot'), 
+            group_chat_created=False, 
+            media_group_id='###', 
+            message_id=###, 
+            photo=(
+                PhotoSize(file_id='###', file_size=1838, file_unique_id='###', height=90, width=90), 
+                PhotoSize(file_id='###', file_size=10214, file_unique_id='###', height=225, width=225)
+            ), 
+            supergroup_chat_created=False
+        )
+    )
+    """
+    return res
